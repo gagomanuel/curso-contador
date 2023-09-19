32156b3d835a7451d454b3e48a3f6f7a41fc9ebe
Gago Manuel <manuel.gago@arba.gov.ar>
2023-09-19 14:26:20 -0300

    midway Boton min3.11.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import freeCodeCampLogo from './imgs/fcc_primary_large.png'
 
 function App() {
   return (
     <div className="App">
-      
+      <nav className='fcc-logo-contain'>
+        <img 
+        className='fcc-logo'
+        src={freeCodeCampLogo}
+        alt='Logo de Free Code Camp'
+        />
+      </nav>
+      <div className='main-contain'>
+        
+      </div>
     </div>
   );
 }
